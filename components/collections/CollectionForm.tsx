23ffb62fca315f5441b50eae43ef2b35cd12e739
Jacob Delott <jldelott@gmail.com>
2024-03-28 18:51:33 -0700

    added compoenents

--- components/collections/CollectionForm.tsx
+++ components/collections/CollectionForm.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+"use client"
+import { Separator } from '../ui/separator'
 
 const CollectionForm = () => {
   return (
     <div className='p-10 '>
         <p className='text-heading2-bold'>Create Collection</p>
+        <Separator className='bg-grey-1 mt-4 mb-7'/>
     </div>
   )
 }
